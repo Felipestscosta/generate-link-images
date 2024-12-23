2d5e8e5bfba44344d1c98f0f74baa1de77cfa711
Felipe Costa <felipestscosta@gmail.com>
2024-12-23 13:59:51 -0300

    fix: correção de titulo da página

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BRK | Auto Cadastro - Expedição ",
+  title: "BRK | Gerador de Links de Imagens",
   description: "Aplicação desenvolvida com o objetivo facilitar nos cadastros de produtos variados.",
 };
 
